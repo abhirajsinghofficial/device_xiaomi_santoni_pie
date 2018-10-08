cd5ce7e11335c1565f12a393c7bcd682583c5e51
Abhiraj <abhirajsinghofficial@gmail.com>
2018-10-08 10:57:56 +0000

    santoni: fix wifi for aosp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -249,6 +249,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_USES_AOSP_WLAN_HAL := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/santoni/BoardConfigVendor.mk
